2b574da8eb5938d2d390c3992063f1c59a00535a
Lionel Cheng <cheng@cerfacs.fr>
2021-03-13 15:39:54 +0100

    Creation of dataframes

--- pytket/span_circuits.py
+++ pytket/span_circuits.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 from pytket.qasm import circuit_from_qasm
 from pytket.passes import (RebaseCirq, RebaseCustom, RebaseHQS, 
@@ -9,17 +11,18 @@ from pytket.passes import (CliffordSimp, SynthesiseIBM, SequencePass,
 from pytket.circuit import Circuit, Unitary1qBox, Unitary2qBox, OpType, Op
 from pytket.extensions.qiskit import tk_to_qiskit
 
-from utils import fig_dir
+from utils import create_dir, fig_dir
 
 def draw_circ(circ:Circuit, filename:str):
     qk_circ = tk_to_qiskit(circ)
     qk_circ.draw(output='mpl', filename=filename)
 
-def convert_circuit(circuit_dir:str, circuit_fn:str, seq_list:list, lprint:bool=False):
+def convert_circuit(circuit_dir:str, circuit_fn:str, seq_list:list, 
+            figname:str, lprint:bool=False):
     circ = circuit_from_qasm(f'{circuit_dir}{circuit_fn}.qasm')
 
     if lprint:
-        draw_circ(circ, f'{fig_dir}{circuit_fn}_base')
+        draw_circ(circ, f'{figname}_base')
 
     to_zx_optimised = SequencePass(seq_list)
 
@@ -27,21 +30,33 @@ def convert_circuit(circuit_dir:str, circuit_fn:str, seq_list:list, lprint:bool=
     to_zx_optimised.apply(circ)
     
     if lprint:
-        draw_circ(circ, f'{fig_dir}{circuit_fn}_opti')
+        draw_circ(circ, f'{figname}_optim')
 
     return circ.depth(), circ.n_gates
 
 if __name__ == '__main__':
     circuit_dir = 'circuits/'
-    circuits_fn = [f'small_{i:d}.qasm' for i in range(1, 5)] + [f'medium_{i:d}.qasm' for i in range(1, 5)]
-    
-    seq_list = [
-        DecomposeBoxes(),
-        RebaseQuil(),
-        EulerAngleReduction(OpType.Rx, OpType.Rz)
-    ]
+    circuits_fn = [f'small_{i:d}' for i in range(1, 5)] + [f'medium_{i:d}' for i in range(1, 5)]
+    ncircuits = len(circuits_fn)
 
-    for circuit_fn in circuits_fn:
-        print(f'Decomposing: {circuit_fn}')
-        depth, n_gates = convert_circuit(circuit_dir, circuit_fn, seq_list, lprint=True)
-        print(f'depth = {depth:d} - n_gates = {n_gates:d}')
+    optims = dict()
+
+    optims['Quil'] = [DecomposeBoxes(), RebaseQuil(), EulerAngleReduction(OpType.Rx, OpType.Rz)]
+    optims['IBM'] = [DecomposeBoxes(), RebaseIBM(), EulerAngleReduction(OpType.Rx, OpType.Rz)]
+    optims['Circ'] = [DecomposeBoxes(), RebaseCirq(), EulerAngleReduction(OpType.Rx, OpType.Rz)]
+
+    df_depth = pd.DataFrame(np.zeros((ncircuits, 3)), index=circuits_fn, columns=optims.keys())
+    df_gates = pd.DataFrame(np.zeros((ncircuits, 3)), index=circuits_fn, columns=optims.keys())
+
+    for optim, seq_list in optims.items():
+        optim_figdir = f'{fig_dir}{optim}/'
+        create_dir(optim_figdir)
+        for circuit_fn in circuits_fn:
+            figname = f'{optim_figdir}{circuit_fn}'
+            depth, ngates = convert_circuit(circuit_dir, circuit_fn, 
+                    seq_list, figname, lprint=True)
+            df_depth[optim][circuit_fn] = depth
+            df_gates[optim][circuit_fn] = ngates
+
+    print(df_depth)
+    print(df_gates)
--- pytket/utils.py
+++ pytket/utils.py
@@ -1,1 +1,8 @@
+import os
+
 fig_dir = 'figures/'
+
+def create_dir(dir_name:str) -> None:
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+        
