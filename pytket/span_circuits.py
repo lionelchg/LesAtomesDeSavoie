35a2128c2916e0df37c6a8c7cb7c3e2f8c67a756
Lionel Cheng <cheng@cerfacs.fr>
2021-03-13 16:20:11 +0100

    Stats plot

--- pytket/span_circuits.py
+++ pytket/span_circuits.py
@@ -32,12 +32,17 @@ def convert_circuit(circuit_dir:str, circuit_fn:str, seq_list:list,
     
     if lprint:
         draw_circ(circ, f'{figname}_optim')
+    
+    n_twogates = (circ.n_gates_of_type(OpType.CZ) + circ.n_gates_of_type(OpType.CX)
+                + circ.n_gates_of_type(OpType.CY) + circ.n_gates_of_type(OpType.H))
 
-    return circ.depth(), circ.n_gates
+    return circ.depth(), circ.n_gates, n_twogates
 
 if __name__ == '__main__':
     circuit_dir = 'circuits/'
     circuits_fn = [f'small_{i:d}' for i in range(1, 5)] + [f'medium_{i:d}' for i in range(1, 5)]
+    circuits_fn += [f'large_{i:d}' for i in range(1, 5)]
+
     ncircuits = len(circuits_fn)
 
     optims = dict()
@@ -48,16 +53,35 @@ if __name__ == '__main__':
 
     df_depth = pd.DataFrame(np.zeros((ncircuits, 3)), index=circuits_fn, columns=optims.keys())
     df_gates = pd.DataFrame(np.zeros((ncircuits, 3)), index=circuits_fn, columns=optims.keys())
+    df_twoq_gates = pd.DataFrame(np.zeros((ncircuits, 3)), index=circuits_fn, columns=optims.keys())
 
     for optim, seq_list in optims.items():
         optim_figdir = f'{fig_dir}{optim}/'
         create_dir(optim_figdir)
         for circuit_fn in circuits_fn:
+            print(f'{optim} + {circuit_fn}')
             figname = f'{optim_figdir}{circuit_fn}'
-            depth, ngates = convert_circuit(circuit_dir, circuit_fn, 
-                    seq_list, figname, lprint=True)
+            depth, ngates, ntwogates = convert_circuit(circuit_dir, circuit_fn, 
+                    seq_list, figname)
             df_depth[optim][circuit_fn] = depth
             df_gates[optim][circuit_fn] = ngates
+            df_twoq_gates[optim][circuit_fn] = ntwogates
 
-    print(df_depth)
-    print(df_gates)
+    print('Depth:\n', df_depth)
+    print('Gates:\n', df_gates)
+    print('Two Gates:\n', df_twoq_gates)
+
+    fig, ax = plt.subplots()
+    df_depth.plot.barh(ax=ax)
+    ax.set_title('Circuit depth')
+    fig.savefig('figures/depth', bbox_inches='tight')
+
+    fig, ax = plt.subplots()
+    df_gates.plot.barh(ax=ax)
+    ax.set_title('Number of gates')
+    fig.savefig('figures/gates', bbox_inches='tight')
+
+    fig, ax = plt.subplots()
+    df_twoq_gates.plot.barh(ax=ax)
+    ax.set_title('Number of two qubits gates')
+    fig.savefig('figures/twogates', bbox_inches='tight')
