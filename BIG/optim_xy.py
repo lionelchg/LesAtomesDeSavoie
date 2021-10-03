20b4c916c9c7948db68b0ece8c1a1f966ca0fc2f
Lionel Cheng <cheng@cerfacs.fr>
2021-10-03 17:58:01 +0200

    Add QAOA mismatch algorithm

---
+++ BIG/optim_xy.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  2 10:43:41 2021
+
+@author: bruno
+"""
+
+import numpy as np
+from scipy.linalg import toeplitz
+from geneticalgorithm import geneticalgorithm as ga
+
+N = 5
+Q = 2
+R = np.zeros((N, N, N))
+
+R[:, :, 0] = np.identity(N)
+for n in range(1, N):
+    R[:, :, n] = toeplitz(np.zeros(N), np.concatenate((np.zeros(n), np.ones(1), np.zeros(N-n-1))))
+
+def rho(x, y, R, k):
+    if k >= 0:
+        return x.T@R[:, :, k]@y
+    else:
+        return y.T@R[:, :, -k]@x
+
+
+
+model = ga(function=islr, dimension=2*N, variable_type='int', variable_boundaries=np.array([[0,Q-1]]*2*N))
+
+model.run()
